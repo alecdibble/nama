2a3694c88b1796d4239776bb9b29a9c500ec9026
Alec Dibble <alecdibble@gmail.com>
2018-11-09 14:12:58 -0800

    Fixed autocomplete

--- src/install.js
+++ src/install.js
@@ -1,8 +1,8 @@
 const tabtab = require('tabtab')
 
 tabtab.install({
-  name: 'na',
-  completer: 'na'
+  name: 'a',
+  completer: 'a'
 })
   .then(() => console.log('Completion installed'))
   .catch(err => console.error(err))
